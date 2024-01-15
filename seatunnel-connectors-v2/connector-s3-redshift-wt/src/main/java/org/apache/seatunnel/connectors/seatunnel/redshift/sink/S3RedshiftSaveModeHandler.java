1d730c3aaf861be449cbe03b937d5fcce1142196
hailin0 <wanghailin@apache.org>
2024-01-15 16:46:58 +0800

    [Hotfix][S3-Redshift] Fix temporary table not created in savemode

--- seatunnel-connectors-v2/connector-s3-redshift-wt/src/main/java/org/apache/seatunnel/connectors/seatunnel/redshift/sink/S3RedshiftSaveModeHandler.java
+++ seatunnel-connectors-v2/connector-s3-redshift-wt/src/main/java/org/apache/seatunnel/connectors/seatunnel/redshift/sink/S3RedshiftSaveModeHandler.java
@@ -48,6 +48,62 @@ public class S3RedshiftSaveModeHandler extends DefaultSaveModeHandler {
         this.redshiftJdbcClient = RedshiftJdbcClient.newSingleConnection(conf);
     }
 
+    @Override
+    protected void recreateSchema() {
+        if (conf.notAppendOnlyMode()) {
+            dropTemporaryTable();
+            createTemporaryTable();
+        }
+        dropTable();
+        createTable();
+    }
+
+    @SneakyThrows
+    @Override
+    protected void createSchemaWhenNotExist() {
+        if (conf.notAppendOnlyMode()) {
+            if (!temporaryTableExists()) {
+                createTemporaryTable();
+            }
+        }
+
+        if (!tableExists()) {
+            createTable();
+        }
+    }
+
+    @Override
+    protected void errorWhenSchemaNotExist() {
+        if (conf.notAppendOnlyMode()) {
+            if (!temporaryTableExists()) {
+                createTemporaryTable();
+            }
+        }
+        super.errorWhenSchemaNotExist();
+    }
+
+    @Override
+    protected void keepSchemaDropData() {
+        if (conf.notAppendOnlyMode()) {
+            if (temporaryTableExists()) {
+                truncateTemporaryTable();
+            }
+        }
+        if (tableExists()) {
+            truncateTable();
+        }
+    }
+
+    @Override
+    protected void errorWhenDataExists() {
+        if (conf.notAppendOnlyMode()) {
+            if (temporaryDataExists()) {
+                truncateTemporaryTable();
+            }
+        }
+        super.errorWhenDataExists();
+    }
+
     @SneakyThrows
     @Override
     public boolean tableExists() {
@@ -55,20 +111,30 @@ public class S3RedshiftSaveModeHandler extends DefaultSaveModeHandler {
     }
 
     @SneakyThrows
+    public boolean temporaryTableExists() {
+        return redshiftJdbcClient.existDataForSql(sqlGenerator.getIsExistTemporaryTableSql());
+    }
+
+    @SneakyThrows
     @Override
     public void dropTable() {
         redshiftJdbcClient.execute(sqlGenerator.getDropTableSql());
+    }
+
+    @SneakyThrows
+    public void dropTemporaryTable() {
         redshiftJdbcClient.execute(sqlGenerator.getDropTemporaryTableSql());
     }
 
     @SneakyThrows
     @Override
     public void createTable() {
-        if (conf.notAppendOnlyMode()) {
-            redshiftJdbcClient.execute(sqlGenerator.getDropTemporaryTableSql());
-            redshiftJdbcClient.execute(sqlGenerator.getCreateTemporaryTableSQL());
-        }
         redshiftJdbcClient.execute(sqlGenerator.getCreateTableSQL());
+    }
+
+    @SneakyThrows
+    public void createTemporaryTable() {
+        redshiftJdbcClient.execute(sqlGenerator.getCreateTemporaryTableSQL());
     }
 
     @SneakyThrows
@@ -78,9 +144,19 @@ public class S3RedshiftSaveModeHandler extends DefaultSaveModeHandler {
     }
 
     @SneakyThrows
+    public void truncateTemporaryTable() {
+        redshiftJdbcClient.execute(sqlGenerator.getCleanTemporaryTableSql());
+    }
+
+    @SneakyThrows
     @Override
     public boolean dataExists() {
         return redshiftJdbcClient.existDataForSql(sqlGenerator.getIsExistDataSql());
+    }
+
+    @SneakyThrows
+    public boolean temporaryDataExists() {
+        return redshiftJdbcClient.existDataForSql(sqlGenerator.getIsExistTemporaryDataSql());
     }
 
     @SneakyThrows
