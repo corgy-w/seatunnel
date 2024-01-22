953b5f5bbcd51b51e004d0140c209b1e8e3dcdbe
hailin0 <wanghailin@apache.org>
2024-01-22 11:17:01 +0800

    Revert "[Feature][JDBC] Add Inceptor data type convertor"
    This reverts commit e31fc80ff863a0e59f102b29ab72f83ee4d43f53.

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/inceptor/InceptorTypeMapper.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/inceptor/InceptorTypeMapper.java
@@ -17,45 +17,101 @@
 
 package org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.inceptor;
 
-import org.apache.seatunnel.api.table.catalog.Column;
-import org.apache.seatunnel.api.table.converter.BasicTypeDefine;
-import org.apache.seatunnel.connectors.seatunnel.common.source.TypeDefineUtils;
+import org.apache.seatunnel.api.table.type.BasicType;
+import org.apache.seatunnel.api.table.type.DecimalType;
+import org.apache.seatunnel.api.table.type.LocalTimeType;
+import org.apache.seatunnel.api.table.type.SeaTunnelDataType;
+import org.apache.seatunnel.common.constants.PluginType;
+import org.apache.seatunnel.common.exception.CommonError;
+import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.DatabaseIdentifier;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.JdbcDialectTypeMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 public class InceptorTypeMapper implements JdbcDialectTypeMapper {
 
-    @Override
-    public Column mappingColumn(BasicTypeDefine typeDefine) {
-        return InceptorTypeConverter.INSTANCE.convert(typeDefine);
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(InceptorTypeMapper.class);
+
+    // Numeric Types
+    private static final String HIVE_TINYINT = "TINYINT";
+    private static final String HIVE_SMALLINT = "SMALLINT";
+    private static final String HIVE_INT = "INT";
+    private static final String HIVE_INTEGER = "INTEGER";
+    private static final String HIVE_BIGINT = "BIGINT";
+    private static final String HIVE_FLOAT = "FLOAT";
+    private static final String HIVE_DOUBLE = "DOUBLE";
+    private static final String HIVE_DOUBLE_PRECISION = "DOUBLE PRECISION";
+    private static final String HIVE_DECIMAL = "DECIMAL";
+    private static final String HIVE_NUMERIC = "NUMERIC";
+    // Date/Time Types
+    private static final String HIVE_TIMESTAMP = "TIMESTAMP";
+    private static final String HIVE_DATE = "DATE";
+    private static final String HIVE_INTERVAL = "INTERVAL";
+    // String Types
+    private static final String HIVE_STRING = "STRING";
+    private static final String HIVE_VARCHAR = "VARCHAR";
+    private static final String HIVE_CHAR = "CHAR";
+    // Misc Types
+    private static final String HIVE_BOOLEAN = "BOOLEAN";
+    private static final String HIVE_BINARY = "BINARY";
+    // Complex Types
+    private static final String HIVE_ARRAY = "ARRAY";
+    private static final String HIVE_MAP = "MAP";
+    // float
+    private static final String HIVE_STRUCT = "STRUCT";
+    private static final String HIVE_UNIONTYPE = "UNIONTYPE";
 
     @Override
-    public Column mappingColumn(ResultSetMetaData metadata, int colIndex) throws SQLException {
-        String columnName = metadata.getColumnLabel(colIndex);
-        String nativeType = metadata.getColumnTypeName(colIndex);
-        int isNullable = metadata.isNullable(colIndex);
-        long precision = metadata.getPrecision(colIndex);
-        int scale = metadata.getScale(colIndex);
-
-        if (Arrays.asList("CHAR", "VARCHAR", "VARCHAR2").contains(nativeType)) {
-            long octetLength = TypeDefineUtils.charTo4ByteLength(precision);
-            precision = Math.max(precision, octetLength);
+    public SeaTunnelDataType<?> mapping(ResultSetMetaData metadata, int colIndex)
+            throws SQLException {
+        String columnType = metadata.getColumnTypeName(colIndex).toUpperCase();
+        int precision = metadata.getPrecision(colIndex);
+        switch (columnType) {
+            case HIVE_TINYINT:
+                return BasicType.BYTE_TYPE;
+            case HIVE_SMALLINT:
+                return BasicType.SHORT_TYPE;
+            case HIVE_INT:
+            case HIVE_INTEGER:
+                return BasicType.INT_TYPE;
+            case HIVE_BIGINT:
+                return BasicType.LONG_TYPE;
+            case HIVE_FLOAT:
+                return BasicType.FLOAT_TYPE;
+            case HIVE_DOUBLE:
+            case HIVE_DOUBLE_PRECISION:
+                return BasicType.DOUBLE_TYPE;
+            case HIVE_DECIMAL:
+            case HIVE_NUMERIC:
+                if (precision > 0) {
+                    return new DecimalType(precision, metadata.getScale(colIndex));
+                }
+                LOG.warn("decimal did define precision,scale, will be Decimal(38,18)");
+                return new DecimalType(38, 18);
+            case HIVE_TIMESTAMP:
+                return LocalTimeType.LOCAL_DATE_TIME_TYPE;
+            case HIVE_DATE:
+                return LocalTimeType.LOCAL_DATE_TYPE;
+            case HIVE_STRING:
+            case HIVE_VARCHAR:
+            case HIVE_CHAR:
+                return BasicType.STRING_TYPE;
+            case HIVE_BOOLEAN:
+                return BasicType.BOOLEAN_TYPE;
+            case HIVE_BINARY:
+            case HIVE_ARRAY:
+            case HIVE_INTERVAL:
+            case HIVE_MAP:
+            case HIVE_STRUCT:
+            case HIVE_UNIONTYPE:
+            default:
+                final String jdbcColumnName = metadata.getColumnName(colIndex);
+                throw CommonError.convertToSeaTunnelTypeError(
+                        DatabaseIdentifier.INCEPTOR, PluginType.SOURCE, columnType, jdbcColumnName);
         }
-
-        BasicTypeDefine typeDefine =
-                BasicTypeDefine.builder()
-                        .name(columnName)
-                        .columnType(nativeType)
-                        .dataType(nativeType)
-                        .nullable(isNullable == ResultSetMetaData.columnNullable)
-                        .length(precision)
-                        .precision(precision)
-                        .scale(scale)
-                        .build();
-        return mappingColumn(typeDefine);
     }
 }
