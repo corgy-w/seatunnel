1903467b477e29ba72137e4ad6297a06261ff903
Eric <gaojun2048@gmail.com>
2024-05-10 16:01:49 +0800

    Fix informix cdc can not found driver bug

--- seatunnel-connectors-v2/connector-cdc/connector-cdc-informix-wt/src/main/java/org/apache/seatunnel/connectors/cdc/informix/source/InformixIncrementalSourceFactory.java
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-informix-wt/src/main/java/org/apache/seatunnel/connectors/cdc/informix/source/InformixIncrementalSourceFactory.java
@@ -73,6 +73,12 @@ public class InformixIncrementalSourceFactory implements TableSourceFactory {
     public <T, SplitT extends SourceSplit, StateT extends Serializable>
             TableSource<T, SplitT, StateT> createSource(TableSourceFactoryContext context) {
         return () -> {
+            // Load the JDBC driver in to DriverManager
+            try {
+                Class.forName("com.informix.jdbc.IfxDriver");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
             List<CatalogTable> catalogTables =
                     CatalogTableUtil.getCatalogTables(
                             context.getOptions(), context.getClassLoader());
