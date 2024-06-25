f8900065599fb98f0e6570b8accfa46b6ca1dd03
老王 <58297137+chl-wxp@users.noreply.github.com>
2024-06-25 11:11:07 +0800

    Feature/2.6 test add highgo cdc connector (#1034)

---
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-highgo-wt/src/main/java/io/debezium/connector/highgo/TruncateRecordEmitter.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.debezium.connector.highgo;
+
+import io.debezium.connector.highgo.connection.HighGoConnection;
+import io.debezium.connector.highgo.connection.ReplicationMessage;
+import io.debezium.pipeline.spi.OffsetContext;
+import io.debezium.relational.TableId;
+import io.debezium.util.Clock;
+
+/**
+ * Description: Message to clear table data
+ *
+ * @author czy
+ * @date 2023/06/02
+ */
+public class TruncateRecordEmitter extends HighGoChangeRecordEmitter {
+    /**
+     * Constructor
+     *
+     * @param partition Partition
+     * @param offset OffsetContext
+     * @param clock Clock
+     * @param connectorConfig HighGoConnectorConfig
+     * @param schema HighGoSchema
+     * @param connection HighGoConnection
+     * @param tableId TableId
+     * @param message ReplicationMessage
+     */
+    public TruncateRecordEmitter(
+            OffsetContext offset,
+            Clock clock,
+            HighGoConnectorConfig connectorConfig,
+            HighGoSchema schema,
+            HighGoConnection connection,
+            TableId tableId,
+            ReplicationMessage message) {
+        super(offset, clock, connectorConfig, schema, connection, tableId, message);
+    }
+}
