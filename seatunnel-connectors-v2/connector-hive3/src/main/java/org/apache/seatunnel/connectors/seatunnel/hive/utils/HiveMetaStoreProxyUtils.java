2ac1b465ef94fe66b3f166a84a3cf190a056c26b
Eric <gaojun2048@gmail.com>
2024-06-26 19:13:54 +0800

    Fix hive bug (#1045)

--- seatunnel-connectors-v2/connector-hive3/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/utils/HiveMetaStoreProxyUtils.java
+++ seatunnel-connectors-v2/connector-hive3/src/main/java/org/apache/seatunnel/connectors/seatunnel/hive/utils/HiveMetaStoreProxyUtils.java
@@ -20,6 +20,8 @@ package org.apache.seatunnel.connectors.seatunnel.hive.utils;
 import org.apache.seatunnel.api.configuration.ReadonlyConfig;
 import org.apache.seatunnel.connectors.seatunnel.file.hdfs.config.HdfsConfigOptions;
 
+import org.apache.commons.lang3.StringUtils;
+
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -27,9 +29,12 @@ public class HiveMetaStoreProxyUtils {
 
     public boolean enableKerberos(ReadonlyConfig config) {
         boolean kerberosPrincipalEmpty =
-                config.getOptional(HdfsConfigOptions.KERBEROS_PRINCIPAL).isPresent();
+                config.getOptional(HdfsConfigOptions.KERBEROS_PRINCIPAL).isPresent()
+                        && StringUtils.isNotBlank(config.get(HdfsConfigOptions.KERBEROS_PRINCIPAL));
         boolean kerberosKeytabPathEmpty =
-                config.getOptional(HdfsConfigOptions.KERBEROS_KEYTAB_PATH).isPresent();
+                config.getOptional(HdfsConfigOptions.KERBEROS_KEYTAB_PATH).isPresent()
+                        && StringUtils.isNotBlank(
+                                config.get(HdfsConfigOptions.KERBEROS_KEYTAB_PATH));
         if (kerberosKeytabPathEmpty && kerberosPrincipalEmpty) {
             return true;
         }
@@ -43,6 +48,7 @@ public class HiveMetaStoreProxyUtils {
     }
 
     public boolean enableRemoteUser(ReadonlyConfig config) {
-        return config.getOptional(HdfsConfigOptions.REMOTE_USER).isPresent();
+        return config.getOptional(HdfsConfigOptions.REMOTE_USER).isPresent()
+                && StringUtils.isNotBlank(config.get(HdfsConfigOptions.REMOTE_USER));
     }
 }
