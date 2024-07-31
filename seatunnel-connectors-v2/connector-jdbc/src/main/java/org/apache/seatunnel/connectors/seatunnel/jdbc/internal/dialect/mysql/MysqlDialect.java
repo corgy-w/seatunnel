d4887e39010ed906804e4851076d653334b88af3
hailin0 <wanghailin@apache.org>
2024-07-31 10:22:35 +0800

    [Improve][Jdbc] Allows to exclude primary key columns when update (#1122)

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/mysql/MysqlDialect.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/internal/dialect/mysql/MysqlDialect.java
@@ -95,6 +95,11 @@ public class MysqlDialect implements JdbcDialect {
             boolean isPrimaryKeyUpdated) {
         String updateClause =
                 Arrays.stream(fieldNames)
+                        .filter(
+                                fieldName ->
+                                        isPrimaryKeyUpdated
+                                                || !Arrays.asList(uniqueKeyFields)
+                                                        .contains(fieldName))
                         .map(
                                 fieldName ->
                                         quoteIdentifier(fieldName)
