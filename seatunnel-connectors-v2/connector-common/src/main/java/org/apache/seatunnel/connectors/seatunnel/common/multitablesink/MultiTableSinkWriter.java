781373426a172ae12f80a487680918e027926e6d
hailin0 <wanghailin@apache.org>
2024-09-19 15:45:11 +0800

    [Hotfix][Core] Fix concurrency exceptions when MultiTableSink#PrepareCommit (#1263)

--- seatunnel-connectors-v2/connector-common/src/main/java/org/apache/seatunnel/connectors/seatunnel/common/multitablesink/MultiTableSinkWriter.java
+++ seatunnel-connectors-v2/connector-common/src/main/java/org/apache/seatunnel/connectors/seatunnel/common/multitablesink/MultiTableSinkWriter.java
@@ -33,6 +33,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -207,7 +208,8 @@ public class MultiTableSinkWriter
     public Optional<MultiTableCommitInfo> prepareCommit() throws IOException {
         checkQueueRemain();
         subSinkErrorCheck();
-        MultiTableCommitInfo multiTableCommitInfo = new MultiTableCommitInfo(new HashMap<>());
+        MultiTableCommitInfo multiTableCommitInfo =
+                new MultiTableCommitInfo(new ConcurrentHashMap<>());
         List<Future<?>> futures = new ArrayList<>();
         for (int i = 0; i < sinkWritersWithIndex.size(); i++) {
             int finalI = i;
@@ -239,6 +241,9 @@ public class MultiTableSinkWriter
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+        }
+        if (multiTableCommitInfo.getCommitInfo().isEmpty()) {
+            return Optional.empty();
         }
         return Optional.of(multiTableCommitInfo);
     }
