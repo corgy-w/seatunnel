eba0cf3ab9df34cd435e172fb08108be41eacaf7
hailin0 <wanghailin@apache.org>
2024-11-09 17:08:42 +0800

    Fix create table comment for dm/oracle/pg (#1400)

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/oracle/OracleCreateTableSqlBuilder.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/oracle/OracleCreateTableSqlBuilder.java
@@ -41,6 +41,7 @@ public class OracleCreateTableSqlBuilder extends AbstractJdbcCreateTableSqlBuild
 
     private List<Column> columns;
     private PrimaryKey primaryKey;
+    private String comment;
     private String sourceCatalogName;
     private String fieldIde;
     private List<ConstraintKey> constraintKeys;
@@ -51,6 +52,7 @@ public class OracleCreateTableSqlBuilder extends AbstractJdbcCreateTableSqlBuild
     public OracleCreateTableSqlBuilder(CatalogTable catalogTable) {
         this.columns = catalogTable.getTableSchema().getColumns();
         this.primaryKey = catalogTable.getTableSchema().getPrimaryKey();
+        this.comment = catalogTable.getComment();
         this.sourceCatalogName = catalogTable.getCatalogName();
         this.fieldIde = catalogTable.getOptions().get("fieldIde");
         constraintKeys = catalogTable.getTableSchema().getConstraintKeys();
@@ -107,6 +109,13 @@ public class OracleCreateTableSqlBuilder extends AbstractJdbcCreateTableSqlBuild
         createTableSql.append(String.join(",\n", columnSqls));
         createTableSql.append("\n)");
         sqls.add(createTableSql.toString());
+        if (comment != null) {
+            StringBuilder commentSql = new StringBuilder();
+            commentSql.append("COMMENT ON TABLE ");
+            commentSql.append(tablePath.getSchemaAndTableName("\""));
+            commentSql.append(" IS '").append(comment).append("'");
+            sqls.add(commentSql.toString());
+        }
         List<String> commentSqls =
                 columns.stream()
                         .filter(column -> StringUtils.isNotBlank(column.getComment()))
