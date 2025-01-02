afb3b38e5be40f9d1f22527c4433e1f68f5d10ed
hailin0 <wanghailin@apache.org>
2025-01-02 10:03:44 +0800

    ST-2584 Fix iceberg table primaryKey is empty (#1592)

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/mysql/MysqlCreateTableSqlBuilder.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/mysql/MysqlCreateTableSqlBuilder.java
@@ -151,7 +151,7 @@ public class MysqlCreateTableSqlBuilder {
             sqls.add("COLLATE = " + collate);
         }
         if (comment != null) {
-            sqls.add("COMMENT = '" + comment + "'");
+            sqls.add("COMMENT = '" + comment.replace("'", "''").replace("\\", "\\\\") + "'");
         }
         return String.join(" ", sqls) + ";";
     }
