d685501a8e6cd01dcd2163e0a02f38d2cc94f501
corgy-w <73771213+corgy-w@users.noreply.github.com>
2025-03-24 18:44:35 +0800

    [Fix][Connector-V2] fix sr connector with dialect (#1813)

--- seatunnel-connectors-v2/connector-jdbc-starrocks/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/source/JdbcStarRocksSourceFactory.java
+++ seatunnel-connectors-v2/connector-jdbc-starrocks/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/source/JdbcStarRocksSourceFactory.java
@@ -17,11 +17,24 @@
 
 package org.apache.seatunnel.connectors.seatunnel.jdbc.source;
 
+import org.apache.seatunnel.api.configuration.ReadonlyConfig;
+import org.apache.seatunnel.api.source.SeaTunnelSource;
+import org.apache.seatunnel.api.source.SourceSplit;
+import org.apache.seatunnel.api.table.connector.TableSource;
 import org.apache.seatunnel.api.table.factory.Factory;
+import org.apache.seatunnel.api.table.factory.TableSourceFactoryContext;
+import org.apache.seatunnel.connectors.seatunnel.jdbc.config.JdbcOptions;
+import org.apache.seatunnel.connectors.seatunnel.jdbc.config.JdbcSourceConfig;
 import org.apache.seatunnel.connectors.seatunnel.jdbc.config.StarRocksIdentifier;
+import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.JdbcDialect;
+import org.apache.seatunnel.connectors.seatunnel.jdbc.internal.dialect.JdbcDialectLoader;
 
 import com.google.auto.service.AutoService;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @AutoService(Factory.class)
@@ -29,5 +42,24 @@ public class JdbcStarRocksSourceFactory extends JdbcSourceFactory {
     @Override
     public String factoryIdentifier() {
         return StarRocksIdentifier.JDBC_STARROCKS;
+    }
+
+    @Override
+    public <T, SplitT extends SourceSplit, StateT extends Serializable>
+            TableSource<T, SplitT, StateT> createSource(TableSourceFactoryContext context) {
+        Map<String, String> configMap = context.getOptions().toMap();
+        configMap.put(JdbcOptions.DIALECT.key(), "StarRocks");
+        ReadonlyConfig readonlyConfig = ReadonlyConfig.fromMap(new HashMap<>(configMap));
+        JdbcSourceConfig config = JdbcSourceConfig.of(readonlyConfig);
+        JdbcDialect jdbcDialect =
+                JdbcDialectLoader.load(
+                        config.getJdbcConnectionConfig().getUrl(),
+                        config.getJdbcConnectionConfig().getCompatibleMode(),
+                        config.getJdbcConnectionConfig().getDialect());
+        jdbcDialect.connectionUrlParse(
+                config.getJdbcConnectionConfig().getUrl(),
+                config.getJdbcConnectionConfig().getProperties(),
+                jdbcDialect.defaultParameter());
+        return () -> (SeaTunnelSource<T, SplitT, StateT>) new JdbcSource(config);
     }
 }
