a8689b480bd799d7bdabfe5024b3053bbfa5c87e
David Zollo <davidzollo365@gmail.com>
2025-11-14 17:31:36 +0800

    [Fix] Fix import exception for SqlServerSourceConfigFactoryTest (#2251)

--- seatunnel-connectors-v2/connector-cdc/connector-cdc-sqlserver/src/test/java/org/apache/seatunnel/connectors/seatunnel/cdc/sqlserver/config/SqlServerSourceConfigFactoryTest.java
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-sqlserver/src/test/java/org/apache/seatunnel/connectors/seatunnel/cdc/sqlserver/config/SqlServerSourceConfigFactoryTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.seatunnel.connectors.seatunnel.cdc.sqlserver.source.config;
+package org.apache.seatunnel.connectors.seatunnel.cdc.sqlserver.config;
 
 import org.junit.jupiter.api.Test;
 
