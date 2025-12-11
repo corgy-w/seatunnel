70a3da79a178a7a90cdbac49e9a1a5d48448a970
corgy-w <73771213+corgy-w@users.noreply.github.com>
2025-12-11 14:02:54 +0800

    [Fix][Zeta] Add synchronized keyword to startJob method to prevent concurrent state issues (#2291)

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/dag/physical/PhysicalPlan.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/dag/physical/PhysicalPlan.java
@@ -296,7 +296,7 @@ public class PhysicalPlan {
         updateJobState(JobStatus.FAILING);
     }
 
-    public void startJob() {
+    public synchronized void startJob() {
         isRunning = true;
         log.info("{} state process is start", getJobFullName());
         stateProcess();
