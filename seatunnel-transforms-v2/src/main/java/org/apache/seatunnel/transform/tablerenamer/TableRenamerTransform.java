5baf94cb3a64ec625760392ecfcfac63f5e09375
corgy-w <73771213+corgy-w@users.noreply.github.com>
2026-01-07 14:07:07 +0800

    [Fix] [Transforms-V2] Update table name retrieval to use schema and table name (#2331)

--- seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/tablerenamer/TableRenamerTransform.java
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/tablerenamer/TableRenamerTransform.java
@@ -98,7 +98,7 @@ public class TableRenamerTransform implements SeaTunnelTransform<SeaTunnelRow> {
 
         List<CatalogTable> outputCatalogTable = new ArrayList<>();
         for (CatalogTable table : inputCatalogTable) {
-            String oldTableName = table.getTablePath().getTableName();
+            String oldTableName = table.getTablePath().getSchemaAndTableName();
             String newTableName = convertName(oldTableName);
             TablePath newTablePath =
                     TablePath.of(
@@ -138,7 +138,7 @@ public class TableRenamerTransform implements SeaTunnelTransform<SeaTunnelRow> {
         if (tablePath == null) {
             return event;
         }
-        String oldTableName = tablePath.getTableName();
+        String oldTableName = tablePath.getSchemaAndTableName();
         String newTableName = tableNameMapping.get(oldTableName);
         if (newTableName == null || newTableName.equals(oldTableName)) {
             return event;
@@ -170,7 +170,7 @@ public class TableRenamerTransform implements SeaTunnelTransform<SeaTunnelRow> {
         if (tablePath == null) {
             return event;
         }
-        String oldTableName = tablePath.getTableName();
+        String oldTableName = tablePath.getSchemaAndTableName();
         String newTableName = tableNameMapping.get(oldTableName);
         if (newTableName == null || newTableName.equals(oldTableName)) {
             return event;
@@ -308,7 +308,7 @@ public class TableRenamerTransform implements SeaTunnelTransform<SeaTunnelRow> {
 
         Set<String> upstreamInputTableNames =
                 inputCatalogTable.stream()
-                        .map(e -> e.getTablePath().getTableName())
+                        .map(e -> e.getTablePath().getSchemaAndTableName())
                         .collect(Collectors.toSet());
 
         List<String> notExistTables =
