576246f881001979add52e60ae2ad2c5efdf5031
corgy-w <73771213+corgy-w@users.noreply.github.com>
2026-01-15 17:44:29 +0800

    ST-3105 [Fix][Transform-V2] Fix MapperTransform input table missing check (#2355)

--- seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/mapper/MapperMultiCatalogTransform.java
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/mapper/MapperMultiCatalogTransform.java
@@ -19,15 +19,21 @@ package org.apache.seatunnel.transform.mapper;
 
 import org.apache.seatunnel.api.configuration.ReadonlyConfig;
 import org.apache.seatunnel.api.table.catalog.CatalogTable;
+import org.apache.seatunnel.api.table.catalog.TablePath;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 import org.apache.seatunnel.api.transform.SeaTunnelTransform;
 import org.apache.seatunnel.transform.common.AbstractMultiCatalogSupportTransform;
+import org.apache.seatunnel.transform.exception.TransformCommonError;
+import org.apache.seatunnel.transform.exception.TransformExceptionUtil;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class MapperMultiCatalogTransform extends AbstractMultiCatalogSupportTransform {
-    private List<MapperConfig.SpecificModify> specificModifies;
 
     public MapperMultiCatalogTransform(
             List<CatalogTable> inputCatalogTables, ReadonlyConfig config) {
@@ -43,5 +49,47 @@ public class MapperMultiCatalogTransform extends AbstractMultiCatalogSupportTran
     @Override
     public String getPluginName() {
         return MapperTransform.PLUGIN_NAME;
+    }
+
+    @Override
+    protected void preCheckConfig(ReadonlyConfig config) {
+        List<MapperConfig.SpecificModify> specificModifies = config.get(MapperConfig.SPECIFIC);
+        if (CollectionUtils.isEmpty(specificModifies)) {
+            return;
+        }
+
+        TransformExceptionUtil.withErrorCheck(
+                getPluginName(),
+                specificModifies.iterator(),
+                modify -> {
+                    TablePath configPath = TablePath.of(modify.getInputName(), true);
+                    boolean found =
+                            inputCatalogTables.stream()
+                                    .anyMatch(
+                                            table -> {
+                                                TablePath inputPath =
+                                                        table.getTableId().toTablePath();
+                                                return inputPath
+                                                                .getTableName()
+                                                                .equals(configPath.getTableName())
+                                                        && (StringUtils.isBlank(
+                                                                        configPath.getSchemaName())
+                                                                || Objects.equals(
+                                                                        inputPath.getSchemaName(),
+                                                                        configPath.getSchemaName()))
+                                                        && (StringUtils.isBlank(
+                                                                        configPath
+                                                                                .getDatabaseName())
+                                                                || Objects.equals(
+                                                                        inputPath.getDatabaseName(),
+                                                                        configPath
+                                                                                .getDatabaseName()));
+                                            });
+
+                    if (!found) {
+                        throw TransformCommonError.cannotFindInputTableError(
+                                getPluginName(), modify.getInputName());
+                    }
+                });
     }
 }
